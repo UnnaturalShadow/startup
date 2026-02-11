b8aaa1320971a5470248bda9b1710a951dd1f086
Shadow <joshuahdecker@gmail.com>
2026-02-11 16:14:35 -0700

    react porting of about page and formatting home page

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -2,8 +2,30 @@ import React from 'react';
 
 export function About() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>about displayed here</div>
+    <main className="about-page">
+      <h1>Welcome to RaidBuddy</h1>
+      <p>
+        RaidBuddy provides guidance for Destiny 2 raids in the form of guides, images, and collaborative, interactive maps.
+        This project is made possible with much thanks to{' '}
+        <a
+          href="https://youtube.com/@soteriaaugur?si=tgIqXwQuYepCH4_5"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          SoteriaAugur
+        </a>{' '}
+        who created the maps and provided the vast majority of the written content for the guides as well. I hope this tool helps
+        you and your fireteam conquer whatever challenge you take on, happy raiding!
+      </p>
+
+      <img src="/raid.svg" alt="Destiny Logo" className="about-logo" />
+
+      <h3>Disclaimer</h3>
+      <p>
+        Please note that RaidBuddy is an independent project and is not affiliated with Bungie or Destiny 2 in any official capacity. All content provided
+        on this site is for educational and informational purposes only. Users are encouraged to support the official game and its developers by purchasing
+        Destiny 2 and any associated content through legitimate channels.
+      </p>
     </main>
   );
 }
