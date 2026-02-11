25a4867cdb71e758636b39e9ba36553e5a974896
Shadow <joshuahdecker@gmail.com>
2026-02-10 22:31:29 -0700

    Got rid of some useless files, and worked on react navigation

---
+++ src/analytics/analytics.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Analytics() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>Analytics displayed here</div>
+    </main>
+  );
+}
