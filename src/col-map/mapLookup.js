194cbfbfe4b9c798ce4949a0031ebc3e38f80737
Shadow <joshuahdecker@gmail.com>
2026-02-25 21:24:09 -0700

    Everything works! I think?

---
+++ src/col-map/mapLookup.js
@@ -0,0 +1,17 @@
+import vogMap from "/vog_map.png";
+// import kfMap from "/kf_map.png";
+// import vowMap from "/vow_map.png";
+import soon from "/soon.jpg";
+
+export const MAPS = {
+  "Vault of Glass": {
+    "Encounter 1": soon,
+    "Encounter 2": vogMap,
+  },
+  "King's Fall": {
+    "Encounter 1": soon,
+  },
+  "Vow of the Disciple": {
+    "Encounter 1": soon,
+  },
+};
